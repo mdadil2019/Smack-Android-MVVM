b236b7a09106017b9ab86e664aeccda5faa21912
Mohammad Adil <mdadil2019@gmail.com>
2018-12-20 00:04:22 +0530

    registraiton viewmodel is done but untested

--- Smack-MVVM/app/src/main/java/com/smack/mdadil2019/smack_mvvm/ui/signup/RegistrationActivity.java
+++ Smack-MVVM/app/src/main/java/com/smack/mdadil2019/smack_mvvm/ui/signup/RegistrationActivity.java
@@ -1,14 +1,28 @@
 package com.smack.mdadil2019.smack_mvvm.ui.signup;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.anonymanager.mdadil2019.smack_mvvm.R;
+import com.smack.mdadil2019.smack_mvvm.data.network.model.CreateUserResponse;
 import com.smack.mdadil2019.smack_mvvm.di.root.MyApp;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -18,11 +32,64 @@ public class RegistrationActivity extends AppCompatActivity {
     ViewModelProvider.Factory mFactory;
 
     RegistrationViewModel registrationViewModel;
+
+
+    @BindView(R.id.textInputUserNameReg)
+    TextInputEditText userNameEt;
+
+    @BindView(R.id.textInputEmailReg)
+    TextInputEditText emailEt;
+
+    @BindView(R.id.textInputPasswordReg)
+    TextInputEditText passwordEt;
+
+    @BindView(R.id.progressBarEmail)
+    ProgressBar pgBarRegistration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         ((MyApp)getApplication()).getApplicationComponent().inject(this);
+        getViewModel();
+        setObservers();
+    }
+
+    private void setObservers() {
+        registrationViewModel.createResponseData.observe(this, new Observer<CreateUserResponse>() {
+            @Override
+            public void onChanged(@Nullable CreateUserResponse createUserResponse) {
+                //save data to preferences
+                registrationViewModel.saveDataToPrefs(createUserResponse);
+
+                //start another activity
+            }
+        });
+
+        registrationViewModel.errorData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                pgBarRegistration.setVisibility(INVISIBLE);
+                Toast.makeText(RegistrationActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.createAccountBtn)void createAccount(){
+        String userName = userNameEt.getText().toString();
+        String password = passwordEt.getText().toString();
+        String email = emailEt.getText().toString();
+        if(registrationViewModel.isInputValid(userName,password,email)){
+            pgBarRegistration.setVisibility(VISIBLE);
+            registrationViewModel.register(userName,password,email);
+
+        }else{
+            Toast.makeText(this, "Input is not valid", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getViewModel() {
+        registrationViewModel = ViewModelProviders.of(this,mFactory).get(RegistrationViewModel.class);
     }
 
 
--- Smack-MVVM/app/src/main/java/com/smack/mdadil2019/smack_mvvm/ui/signup/RegistrationViewModel.java
+++ Smack-MVVM/app/src/main/java/com/smack/mdadil2019/smack_mvvm/ui/signup/RegistrationViewModel.java
@@ -1,14 +1,27 @@
 package com.smack.mdadil2019.smack_mvvm.ui.signup;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.smack.mdadil2019.smack_mvvm.data.network.CreateUserService;
 import com.smack.mdadil2019.smack_mvvm.data.network.LoginService;
 import com.smack.mdadil2019.smack_mvvm.data.network.RegistrationService;
 import com.smack.mdadil2019.smack_mvvm.data.network.model.CreateUserRequest;
+import com.smack.mdadil2019.smack_mvvm.data.network.model.CreateUserResponse;
 import com.smack.mdadil2019.smack_mvvm.data.network.model.LoginRequest;
+import com.smack.mdadil2019.smack_mvvm.data.network.model.LoginResponse;
 import com.smack.mdadil2019.smack_mvvm.data.network.model.RegistrationRequest;
 import com.smack.mdadil2019.smack_mvvm.data.prefs.AppPreferencesHelper;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.SingleSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
 
 public class RegistrationViewModel extends ViewModel {
 
@@ -23,6 +36,9 @@ public class RegistrationViewModel extends ViewModel {
     String avatarName;
     String avatarColor;
 
+    MutableLiveData<CreateUserResponse> createResponseData;
+    MutableLiveData<String> errorData;
+
     public RegistrationViewModel(RegistrationRequest regRequest, RegistrationService regService, LoginService loginSer,
                                  LoginRequest loginReq, CreateUserRequest createUserReq, CreateUserService createUserSer,
                                  AppPreferencesHelper prefs){
@@ -33,5 +49,73 @@ public class RegistrationViewModel extends ViewModel {
         createUserRequest = createUserReq;
         createUserService = createUserSer;
         sharedPreferences = prefs;
+
+        createResponseData= new MutableLiveData<>();
+        errorData = new MutableLiveData<>();
+    }
+
+    public void register(final String userName, final String password, final String email) {
+            registrationRequest.setEmail(email);
+            registrationRequest.setPassword(password);
+            Observable<CreateUserResponse> response = registrationService.registerRequest(registrationRequest)
+                    .concatMap(new Function<ResponseBody, ObservableSource<? extends LoginResponse>>() {
+                        @Override
+                        public ObservableSource<? extends LoginResponse> apply(ResponseBody responseBody) throws Exception {
+                            if(responseBody.string().contains("Successfully")){
+                                loginRequest.setPassword(password);
+                                loginRequest.setEmail(email);
+                                return loginService.loginRequest(loginRequest).toObservable();
+                            }
+                            return null;
+                        }
+                    }).concatMap(new Function<LoginResponse, ObservableSource<? extends CreateUserResponse>>() {
+                        @Override
+                        public ObservableSource<? extends CreateUserResponse> apply(LoginResponse loginResponse) throws Exception {
+                            String token = loginResponse.getToken(); //save the token in shared preferences
+                            sharedPreferences.setAuthToken(token);
+                            createUserRequest.setEmail(email);
+                            createUserRequest.setName(userName);
+                            createUserRequest.setAvatarColor(avatarColor);
+                            avatarName = "dark" + (userName.length() % 27);
+                            createUserRequest.setAvatarName(avatarName);
+                            return createUserService.createUser(createUserRequest,"Bearer " + token,"application/json; charset=utf-8");
+                        }
+                    });
+            response.subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<CreateUserResponse>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+                }
+
+                @Override
+                public void onNext(CreateUserResponse createUserResponse) {
+                    createResponseData.setValue(createUserResponse);
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+
+                }
+            });
+    }
+
+    public void saveDataToPrefs(CreateUserResponse createUserResponse){
+        sharedPreferences.saveId(createUserResponse.getId());
+        sharedPreferences.saveUserName(createUserResponse.getName());
+        sharedPreferences.saveAvatarColor(createUserResponse.getAvatarColor());
+        sharedPreferences.saveAvatarName(createUserResponse.getAvatarName());
+    }
+
+
+    public boolean isInputValid(String userName, String password, String email) {
+        if(userName!=null && !userName.equals("") && password!=null && !password.equals("") && email!=null && !email.equals(""))
+            return true;
+        return false;
     }
 }
